5375761f927ac8a2fe5d35925a18b302cf64a2e2
goem987 <oemuer.goez@gmail.com>
2013-12-06 20:09:33 +0100

    Mein Konto
    Verbleibende Laufzeit/Kontingent

--- main/mein_konto.php
+++ main/mein_konto.php
@@ -1,1 +1,5 @@
-Mein Konto
+<h2>Mein Konto</h2>
+<div class="pull-right">
+	<p>Ihre Abolaufzeit: <span class="badge">23 Tage</span></p>
+	<p>Ihr Abokontingent: <span class="badge">7 Lektionen</span></p>
+</div>
