140e8b1b5c87b2e0234650b63f8bb342f5adb182
goem987 <oemuer.goez@gmail.com>
2013-12-06 20:52:55 +0100

    Sidebar fix

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <div class="list-group">
-            <a href="#" class="list-group-item active">Link</a>
+            <a href="#" class="list-group-item">Link</a>
             <a href="#" class="list-group-item">Link</a>
             <a href="#" class="list-group-item">Link</a>
 
