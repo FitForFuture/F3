eeaffd47088316c7da0114bd4d5bb88363088313
goem987 <oemuer.goez@gmail.com>
2013-12-06 20:52:33 +0100

    Mein Konto

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
 <head>
 <title>FitForFun</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
 <!-- Bootstrap -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet"> <!-- individuelle CSS --> 
--- main/mein_konto.php
+++ main/mein_konto.php
@@ -1,5 +1,85 @@
-<h2>Mein Konto</h2>
-<div class="pull-right">
-	<p>Ihre Abolaufzeit: <span class="badge">23 Tage</span></p>
-	<p>Ihr Abokontingent: <span class="badge">7 Lektionen</span></p>
+<div class="row">
+	<div class=" col-md-12">
+	<h2>Mein Konto</h2>
 </div>
+</div>
+<div class="row">
+<div class="col-md-6">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Abonnement-Einstellungen</h3>
+    </div>
+    <div class="panel-body">
+	
+		<p><a class="btn btn-default" href="#">Eigene Daten einsehen u. bearbeiten</a></p>
+		<p><a class="btn btn-default" href="#">Benachrichtigungseinstellungen ändern</a></p>
+    </div>
+  </div>
+</div>
+
+
+<div class="col-md-6">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Abonnement-Kapazität</h3>
+    </div>
+    <div class="panel-body">
+		<p>Ihre Abolaufzeit: <strong>23 Tage</strong></p><p> Ihr Abokontingent: <strong>7 Lektionen</strong></p>
+		
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+<div class="row">
+<div class="col-md-6 ">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Benachrichtigungs-Einstellung</h3>
+    </div>
+    <div class="panel-body">
+   <p>Wählen Sie Art der Benachrichtigungsform in der wir Sie kontaktieren dürfen: </p>
+      <p class="">
+        <input type="radio"> Telefonisch 
+      </p>
+
+      <p class="">
+        <input type="radio"> Per E-Mail 
+      </p>
+
+      <a class="btn btn-default pull-right" href"#">Speichern</a>
+
+      
+    </div>
+  	
+  
+
+    </div>
+  </div>
+
+
+  <div class="col-md-6 ">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Benachrichtigungs-Einstellung</h3>
+    </div>
+    <div class="panel-body">
+   <p><a class="btn btn-default" href="#">Initial Link</a></p>
+		<p><a class="btn btn-default" href="#">Initial Link 2</a></p>
+      
+      
+    </div>
+  	
+  
+
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
